56f2fc3d8adfe5a503e0a170a1821403b1ca2c83
artem <agasyan222@gmail.com>
2024-11-15 00:03:21 +0400

    docker fix

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -2,6 +2,6 @@
 python manage.py makemigrations
 python manage.py migrate
 
-python manage.py createsuperuser --noinput --email admin@example.com --surname Agasyan --name Artem --patronymic Armenovich
+python manage.py createsuperuser --noinput --email admin@example.com --surname Agasyan --name Artem
 
 exec python manage.py runserver 0.0.0.0:8000
